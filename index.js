b99db7be6485b636b300ce324e0d88e9c8d46304
Ahnaf Ahmad <ahrmada95@gmail.com>
2022-07-06 12:59:24 -0400

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Add your code here
+const submitData = (userName, userEmail) => {
+    const configurationObject = {
+        method: "POST", 
+        headers: { 
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        }),
+    }
+
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(response => response.json())
+    .then(data => {
+        let x = document.createElement('h3');
+        x.textContent = data.id;
+        console.log(x)
+        document.body.append(x); 
+    })
+    .catch((error) => {
+        let y = document.createElement('p');
+        y.textContent = error.message;
+        console.log(error.message);
+        document.body.append(y);
+    });
+}
+
+// // Add your code here
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify({
+//         dogName: "Fenrir",
+//         dogBreed: "German-Shepard",
+//     }),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject);
